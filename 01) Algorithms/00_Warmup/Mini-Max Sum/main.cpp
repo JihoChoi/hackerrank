136a6e1ab8a9b80845d26ae01fd760e0d75a1d5b
Jiho Choi <choi296@purdue.edu>
2017-03-24 03:31:49 -0400

    warmup mini max

---
+++ 01) Algorithms/00_Warmup/Mini-Max Sum/main.cpp
@@ -0,0 +1,46 @@
+
+/*
+	Mini-Max Sum
+	https://www.hackerrank.com/challenges/mini-max-sum
+*/
+
+#include <stdio.h>
+
+
+long int minimum(long int a, long int b) {
+	return a <= b ? a : b;
+}
+
+long int maximum(long int a, long int b) {
+	return a > b ? a : b;
+}
+
+int main() {
+
+    long int a; 
+    long int b; 
+    long int c; 
+    long int d;
+    long int e;
+    scanf("%ld %ld %ld %ld %ld",&a,&b,&c,&d,&e);
+
+	long int sum = a + b + c + d + e;
+    long int min = a;
+    long int max = a;
+
+	min = minimum(min, b);
+	min = minimum(min, c);
+	min = minimum(min, d);
+	min = minimum(min, e);
+
+	max = maximum(max, b);
+	max = maximum(max, c);
+	max = maximum(max, d);
+	max = maximum(max, e);
+
+	printf("%ld %ld\n", sum - max, sum - min);
+    
+    return 0;
+}
+
+
---
+++ template/template.cpp
@@ -0,0 +1,33 @@
+
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+
+int main() {
+
+
+
+
+	return 0;
+}
+
